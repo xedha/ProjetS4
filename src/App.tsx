096c55ed9e30579a943c2a9296f53495e3451899
amine jim <147877958+amnabb5@users.noreply.github.com>
2025-04-09 20:57:24 +0100

    course-settings-exmas-pages

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import Page from "./Course & Module Management/page"
+import Page1 from "./exams/page1"
+import Page2 from "./settin/page2"
+function App() {
+ 
+
+  return (
+  
+    <>
+
+   
+ <Page/>
+   
+
+    </>
+  )
+}
+
+
+
+
+export default App
+
+
---
+++ src/Course & Module Management/tabel1/deletButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import styles from './Tabel.module.css'
+import modify from './src/assets/edit.svg'
+ function Button(){
+
+return(
+
+
+  
+  <div className={styles.uiverse}>
+      <span className={styles.tooltip}>are you sure !</span>
+      <span>
+          
+      </span>
+  </div>
+
+
+)
+
+
+
+ }
+ export default Button

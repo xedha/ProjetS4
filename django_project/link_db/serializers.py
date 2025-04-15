10381f23a3b756cb703abe3a3000a421a32c8184
NisBenz <benazzaanis783@gmail.com>
2025-04-15 13:16:06 +0200

    rectifying error in django folder

---
+++ django_project/link_db/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User  # Using the existing User table
+from .models import Enseignants, Formations, ChargesEnseignement, Surveillance
+
+class EnseignantsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enseignants
+        fields = '__all__'
+
+class FormationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Formations
+        fields = '__all__'
+
+class ChargesEnseignementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChargesEnseignement
+        fields = '__all__'
+
+class SurveillanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Surveillance
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User  # Using the existing database
+        fields = ['id', 'email', 'password']
+
+    def create(self, validated_data):
+        user, created = User.objects.get_or_create(
+            email=validated_data['email']
+        )
+        if created:
+            user.set_password(validated_data['password'])
+            user.save()
+        return user
